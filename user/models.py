dbf58092c11bc88f14046db0f6e95a51f0536194
brs14shn <brsshn292929@gmail.com>
2022-09-07 10:46:27 +0300

    appname changed name

--- user/models.py
+++ user/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+# Customize User
 class UserProfile(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to="profile_pics/",blank=True)
     bio = models.TextField(blank=True)
-
 
     def __str__(self):
         return self.user.username
