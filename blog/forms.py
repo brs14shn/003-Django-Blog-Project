98a098c2ebd88a36c9107ce4c8395b76bc055844
brs14shn <brsshn292929@gmail.com>
2022-09-09 11:09:52 +0300

    models and forms created

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from blog.models import Post, Comment
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=('title','content','post_image','category','status')
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=('comment',)
+        labels={'comment':'Comment'}
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+CATEGORY = (
+    (None, 'Choose...'),
+    ('Technology','Technology'),
+    ('Software','Software'),
+    ('Spor','Spor'),
+    ('Fashion','Fashion'),
+    ('Lifestyle','Lifestyle'),
+    ('Travel','Travel'),
+    ('Food','Food'),
+)
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+#!-----------------------POST---------------------------
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    post_image=models.ImageField(upload_to='post_image', blank=True)
+    category=models.CharField(max_length=50,choices=CATEGORY, blank=True,verbose_name="category")
+    publish_date=models.DateTimeField(auto_now_add=True)
+    last_update=models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    blog_views = models.PositiveIntegerField(default=0)
+    blog_like = models.PositiveIntegerField(default=0)
+    blog_comment = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+
+#!-----------------------COMMENT---------------------------
+class Comment(models.Model):
+    comment = models.TextField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['date_added']
+
+    def __str__(self):
+        return self.comment
+
+#!-----------------------LİKE---------------------------
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+
+   
