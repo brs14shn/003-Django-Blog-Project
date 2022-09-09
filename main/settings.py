2e7da887f380657d7ca94ae95d182c888fde5c94
brs14shn <brsshn292929@gmail.com>
2022-09-09 11:21:30 +0300

    admin register

--- main/settings.py
+++ main/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+#? STATİC
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'blog/static/'
+
+
+#? MEDİA
 MEDIA_URL="media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
